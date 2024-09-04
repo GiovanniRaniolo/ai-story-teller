134cd1a428893f21a3ea4876b1bf43b9adaebf96
Giovanni Raniolo <gianniraniolo80@gmail.com>
2024-09-04 19:18:23 +0200

    feat: add Switch, SwitchBox components and Gemini Api calls

--- src/constants/common.ts
+++ src/constants/common.ts
@@ -24,3 +24,46 @@ export const listaGeneri = [
     value: "giallo",
   },
 ];
+
+export const listaStiliNarrativi = [
+  {
+    label: "Prima Persona",
+    value: "prima_persona",
+  },
+  {
+    label: "Terza Persona",
+    value: "terza_persona",
+  },
+  {
+    label: "Narratore Onnisciente",
+    value: "narratore_onnisciente",
+  },
+  {
+    label: "Stile Diario",
+    value: "stile_diario",
+  },
+  {
+    label: "Flusso di Coscienza",
+    value: "flusso_di_coscienza",
+  },
+  {
+    label: "Epistolare (sotto forma di lettere)",
+    value: "epistolare",
+  },
+  {
+    label: "Stile Cinematografico",
+    value: "stile_cinematografico",
+  },
+  {
+    label: "Stile Fiabesco",
+    value: "stile_fiabesco",
+  },
+  {
+    label: "Stile Poetico",
+    value: "stile_poetico",
+  },
+  {
+    label: "Stile Giornale di Bordo",
+    value: "stile_giornale_di_bordo",
+  },
+];
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,16 +5,72 @@ import WindowBox from "@/components/Organism/WindowBox/WindowBox";
 import InputBox from "@/components/Molecules/InputBox/InputBox";
 import SelectBox from "@/components/Molecules/SelectBox/SelectBox";
 import { useState } from "react";
-import { listaGeneri } from "@/constants/common";
+import { listaGeneri, listaStiliNarrativi } from "@/constants/common";
 import Button from "@/components/Atoms/Button/Button";
+import {
+  GenerateContentCandidate,
+  GoogleGenerativeAI,
+} from "@google/generative-ai";
+import SwitchBox from "@/components/Molecules/SwitchBox/SwitchBox";
 
 export default function Home() {
   const [protagonista, setProtagonista] = useState("");
   const [antagonista, setAntagonista] = useState("");
   const [genere, setGenere] = useState("");
+  const [personaggiSecondari, setPersonaggiSecondari] = useState(false);
+  const [stileNarrativo, setStileNarrativo] = useState("");
+  const [breve, setBreve] = useState(false); // Cambiato da "dettagliata" a "breve"
+  const [loading, setLoading] = useState(false);
+  const [response, setResponse] = useState("");
 
-  const handleGenerate = () => {
-    console.log({ protagonista, antagonista, genere });
+  const handleGenerate = async () => {
+    setLoading(true);
+    const prompt = `genera un racconto ${genere} ${
+      breve ? "breve" : "dettagliato"
+    }, con il protagonista chiamato ${protagonista}, l'antagonista chiamato ${antagonista}${
+      personaggiSecondari ? ", aggiungi uno o più personaggi secondari" : ""
+    }. 
+    Stile narrativo: ${stileNarrativo}.`;
+
+    console.log("Prompt generato:", prompt); // Debug
+
+    try {
+      if (!process.env.NEXT_PUBLIC_API_KEY) {
+        console.error("API_KEY non è definita!"); // Debug
+        return;
+      }
+
+      if (
+        protagonista.trim().length <= 0 ||
+        antagonista.trim().length <= 0 ||
+        genere.trim().length <= 0
+      ) {
+        setLoading(false);
+        return;
+      }
+
+      const genAI = new GoogleGenerativeAI(process.env.NEXT_PUBLIC_API_KEY);
+      const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
+      const result = await model.generateContent(prompt);
+      console.log("Response from API:", result); // Debug
+
+      const output = (
+        result.response.candidates as GenerateContentCandidate[]
+      )[0]?.content?.parts[0]?.text;
+
+      if (output) {
+        console.log("Output generated:", output); // Debug
+        setResponse(output);
+      } else {
+        console.error("No output generated.");
+        setResponse("Errore nella generazione del contenuto.");
+      }
+    } catch (error) {
+      console.error("Errore durante la generazione:", error); // Debug
+      setResponse("Si è verificato un errore durante la generazione.");
+    }
+
+    setLoading(false);
   };
 
   return (
@@ -43,23 +99,49 @@ export default function Home() {
                 value={antagonista}
                 setValue={setAntagonista}
               />
-            </div>
-            <div className={style.container}>
               <SelectBox
                 label="Genere:"
                 list={listaGeneri}
                 setAction={setGenere}
               />
+              <SwitchBox
+                label="Personaggi Secondari:"
+                value={personaggiSecondari}
+                setValue={setPersonaggiSecondari}
+              />
+              <SelectBox
+                label="Stile Narrativo:"
+                list={listaStiliNarrativi}
+                setAction={setStileNarrativo}
+              />
+              <SwitchBox
+                label="Storia Breve:" // Cambiato da "Storia Dettagliata" a "Storia Breve"
+                value={breve}
+                setValue={setBreve}
+              />
+              <Button
+                label="Genera"
+                onClick={handleGenerate}
+                disabled={
+                  protagonista.trim().length <= 0 ||
+                  antagonista.trim().length <= 0 ||
+                  genere.trim().length <= 0 ||
+                  loading
+                }
+              />
             </div>
-            <Button
-              label="Genera"
-              onClick={handleGenerate}
-              disabled={
-                protagonista.trim().length <= 0 ||
-                antagonista.trim().length <= 0 ||
-                genere.trim().length <= 0
-              }
-            />
+
+            {loading ? (
+              <div className={style.loading}>
+                <p>loading...</p>
+              </div>
+            ) : (
+              <div className={style.result}>
+                {response.split("\n\n").map((paragraph, index) => (
+                  <p key={index}>{paragraph}</p>
+                ))}
+              </div>
+            )}
           </WindowBox>
         </div>
       </main>
