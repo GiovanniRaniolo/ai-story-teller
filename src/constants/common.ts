2ccd8630807d2d751cc152588ba0b288836e03b0
Giovanni Raniolo <gianniraniolo80@gmail.com>
2024-09-05 19:08:13 +0200

    feat: Expand genres and narrative styles
    - Added new genres
    - Sorted genres alphabetically
    - Expanded and alphabetically sorted narrative styles with additional styles

--- src/constants/common.ts
+++ src/constants/common.ts
@@ -1,69 +1,45 @@
 export const listaGeneri = [
-  {
-    label: "Horror",
-    value: "horror",
-  },
-  {
-    label: "Fantasy",
-    value: "fantasy",
-  },
-  {
-    label: "Avventura",
-    value: "avventura",
-  },
-  {
-    label: "Romantico",
-    value: "romantico",
-  },
-  {
-    label: "Fantascienza",
-    value: "fantascienza",
-  },
-  {
-    label: "Giallo",
-    value: "giallo",
-  },
+  { label: "Avventura", value: "avventura" },
+  { label: "Avventura Urbana", value: "avventura-urbana" },
+  { label: "Biografico", value: "biografico" },
+  { label: "Classico", value: "classico" },
+  { label: "Commedia", value: "commedia" },
+  { label: "Caper", value: "caper" },
+  { label: "Cyberpunk", value: "cyberpunk" },
+  { label: "Drammatico", value: "drammatico" },
+  { label: "Distopico", value: "distopico" },
+  { label: "Fantasy", value: "fantasy" },
+  { label: "Fiaba per Bambini", value: "fiaba-bambini" },
+  { label: "Fiction Storica", value: "fiction-storica" },
+  { label: "Giallo", value: "giallo" },
+  { label: "Guerra", value: "guerra" },
+  { label: "Mitologia", value: "mitologia" },
+  { label: "Mistero", value: "mistero" },
+  { label: "Narrativa Contemporanea", value: "narrativa-contemporanea" },
+  { label: "Noir", value: "noir" },
+  { label: "Paranormale", value: "paranormale" },
+  { label: "Poesia", value: "poesia" },
+  { label: "Racconto di Viaggio", value: "racconto-viaggio" },
+  { label: "Romantico", value: "romantico" },
+  { label: "Satira", value: "satira" },
+  { label: "Science Fantasy", value: "science-fantasy" },
+  { label: "Spionaggio", value: "spionaggio" },
+  { label: "Storico", value: "storico" },
+  { label: "Surreale", value: "surreale" },
+  { label: "Thriller", value: "thriller" },
 ];
 
 export const listaStiliNarrativi = [
-  {
-    label: "Prima Persona",
-    value: "prima_persona",
-  },
-  {
-    label: "Terza Persona",
-    value: "terza_persona",
-  },
-  {
-    label: "Narratore Onnisciente",
-    value: "narratore_onnisciente",
-  },
-  {
-    label: "Stile Diario",
-    value: "stile_diario",
-  },
-  {
-    label: "Flusso di Coscienza",
-    value: "flusso_di_coscienza",
-  },
-  {
-    label: "Epistolare (sotto forma di lettere)",
-    value: "epistolare",
-  },
-  {
-    label: "Stile Cinematografico",
-    value: "stile_cinematografico",
-  },
-  {
-    label: "Stile Fiabesco",
-    value: "stile_fiabesco",
-  },
-  {
-    label: "Stile Poetico",
-    value: "stile_poetico",
-  },
-  {
-    label: "Stile Giornale di Bordo",
-    value: "stile_giornale_di_bordo",
-  },
+  { label: "Diario", value: "diario" },
+  { label: "Epistolare", value: "epistolare" },
+  { label: "Flusso di Coscienza", value: "flusso_di_coscienza" },
+  { label: "Giornale di Bordo", value: "giornale_di_bordo" },
+  { label: "Narratore Onnisciente", value: "narratore_onnisciente" },
+  { label: "Prima Persona", value: "prima_persona" },
+  { label: "Prosa Fluida", value: "prosa_fluida" },
+  { label: "Stile Cinematografico", value: "stile_cinematografico" },
+  { label: "Stile Fiabesco", value: "stile_fiabesco" },
+  { label: "Stile Giornale di Bordo", value: "stile_giornale_di_bordo" },
+  { label: "Stile Poetico", value: "stile_poetico" },
+  { label: "Terza Persona", value: "terza_persona" },
 ];
